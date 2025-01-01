c297e8322c5cfd662234208196ab49c51897a494
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-01 11:23:47 +0530

    Adding all the initial files and folder

---
+++ src/main/java/com/easy_bytes_spring_security/EazyBytes_Spring_Security/EazyBytesSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.easy_bytes_spring_security.EazyBytes_Spring_Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazyBytesSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazyBytesSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easy_bytes_spring_security/EazyBytes_Spring_Security/EazyBytesSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy_bytes_spring_security.EazyBytes_Spring_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBytesSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
